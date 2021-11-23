8987bd297a58b390925ee6fa0d4f58e5773ae341
Jackson Argo <jackson@jacksonargo.com>
2021-11-22 21:56:55 -0500

    add idea and text extractor

---
+++ bin/extract_text.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+
+workDir=$(pwd)
+while ! [ -d data ]; do
+  cd ..
+  if [ "$workDir" -eq "$(pwd)" ]; then
+    echo "could not find data dir"
+    exit 1
+  fi
+done
+
+for file in data/src/*.pdf; do
+  output="data/output/$(basename "$file" .pdf | cksum | awk '{print $1}').txt"
+  pdftotext "$file" "$output"
+done
